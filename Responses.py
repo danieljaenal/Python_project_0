757528b3cdecbc1f969cddeb5fe5eebbabcf9c96
danieljaenal <54280631+danieljaenal@users.noreply.github.com>
2022-12-04 15:32:16 +0100

    movido al chat del bot 100% funcional

--- Responses.py
+++ Responses.py
@@ -18,8 +18,8 @@ def city_weather(city):
     api_key = '3886d5996f84c5b9a5c953b027fc2306'
       
     # city= urllib.parse.quote(input("Introduce la ciudad: "))
-    city=urllib.parse.quote(city)
-    url2= url.replace("{city name}", city).replace("{API key}", api_key)
+    city_uncoded=urllib.parse.quote(city)
+    url2= url.replace("{city name}", city_uncoded).replace("{API key}", api_key)
     # print(url2)
       
     data= requests.get(url2)
@@ -34,13 +34,17 @@ def city_weather(city):
       
     ts2 = [datetime.strptime(d, '%Y-%m-%d %H:%M:%S') for d in fechas]
     fig= plt.figure(figsize=(12, 4))
-    plt.grid()
+    plt.grid()   
+    plt.ylabel("ºC")
+    titulo_grafico= "Pronóstico temperatura en " + str(city)
+    plt.title(titulo_grafico)
       
     plt.plot(ts2, temperatura, color="blue");
     plt.savefig("temperatura.png")
       
     files = {'photo': open('temperatura.png', 'rb')}  
-    url= 'https://api.telegram.org/bot5894955616:AAG_4S2XLv9Wx4BhGTC3uykjXWnzwigggj4/sendPhoto?chat_id=-1001822743230'
+    # url= 'https://api.telegram.org/bot5894955616:AAG_4S2XLv9Wx4BhGTC3uykjXWnzwigggj4/sendPhoto?chat_id=-1001822743230'
+    url= 'https://api.telegram.org/bot5894955616:AAG_4S2XLv9Wx4BhGTC3uykjXWnzwigggj4/sendPhoto?chat_id=132274401'
     url_caption = url + '&caption=' + 'El tiempo en ' + str(city)
     resp= requests.get(url_caption, files = files)
       
@@ -56,17 +60,18 @@ def sample_responses (input_text):
     cities=available_cities.check_city()
     
     if user_message in cities:
+        # user_message= urllib.parse.quote(user_message) #aquí traduzco el nombre de la ciudad a código que entiende la máquina
         city_weather(city=user_message)
         # mensaje= "Aquí tienes el tiempo en " + str(user_message)
-        # return mensaje
 
     elif user_message not in cities:
         return "City not found, please try another one"
     
-    if user_message in ("time", "time?"):
+    elif user_message in ("time", "time?"):
         new = datetime.now()
         date_time= now.strftime("%d/%m/%y, %H:%M:%S")
         return str(date_time)
     
     # print("llegué")
-    return "I don't understand you"
+    else:
+        return "I don't understand you"
